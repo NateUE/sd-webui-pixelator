8d52f8a564299853e60ef87d24bf7ae3c075032d
Nate Swearingen <nathanial.swearingen@gmail.com>
2024-06-23 16:33:14 -0700

    Update and rename pixel_fix.py to pixelator.py
    Translated all to English.
    Updated slider UI for clarity.
    Changed image name output basename to suffix with reference to selected pixel size. This allows images to appear next to each other in the output folder when sorted by name.

--- scripts/pixelator.py
+++ scripts/pixelator.py
@@ -6,46 +6,45 @@ from modules.shared import opts
 
 
 class Script(scripts.Script):
-    print('像素修复插件已加载...')
+    print('Pixelator plugin loaded...')
 
     def title(self):
-        return "像素修复"
+        return "Pixelator"
 
-    # 显示插件界面
+    # Show plug-in interface
     def show(self, is_img2img):
         return scripts.AlwaysVisible
 
-    # 加载UI
+    # Load UI
     def ui(self, is_img2img):
-        with gr.Accordion("像素修复", open=False):
+        with gr.Accordion("Pixelator", open=False):
             with gr.Row():
-                enabled = gr.Checkbox(label="Enable(是否启用)", value=False)
+                enabled = gr.Checkbox(label="Enable", value=False)
 
             with gr.Column():
                 with gr.Row():
-                    pixel_size = gr.Slider(label="Pixel Size recommend：8(像素颗粒大小，推荐值：8)", minimum=1, maximum=32,
-                                           step=1, value=8)
+                    pixel_size = gr.Slider(minimum=1, maximum=32, step=1, value=6, label="Pixel size", info="1 = No change, recommended values：2-8")
 
         return [enabled, pixel_size]
 
-    # 读取图片进程
+    # Reading image process
     def postprocess(self, p, processed, enabled, pixel_size):
 
-        # 是否启用
+        # Whether to enable
         if not enabled:
             return
 
-        # 简单的像素对齐处理
+        # Simple pixel alignment
         def process_image(original_image):
             small = original_image.resize((original_image.width // pixel_size, original_image.height // pixel_size),
                                           resample=Image.NEAREST)
             return small.resize((original_image.width, original_image.height), resample=Image.NEAREST)
 
-        # 批量处理
+        # Batch processing
         for i in range(len(processed.images)):
             pixel_image = process_image(processed.images[i])
             processed.images.append(pixel_image)
-            images.save_image(pixel_image, p.outpath_samples, "pixel",
-                              processed.seed + i, processed.prompt, opts.samples_format, info=processed.info, p=p)
+            images.save_image(pixel_image, p.outpath_samples, "",
+                              processed.seed + i, processed.prompt, opts.samples_format, info=processed.info, p=p, suffix=f"-PixelSize-{pixel_size}")
 
         return processed
